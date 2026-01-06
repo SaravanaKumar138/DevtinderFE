9e88b413a05ab87529489775af16aa238248ba5d
Saravana <godsaran05@gmail.com>
2026-01-06 12:41:44 +0530

    added userId to socket

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,12 +1,22 @@
-
 import { io } from "socket.io-client";
+import store from "../store";
 import { url } from "./constants";
+import { useSelector } from "react-redux";
 
 const createSocketConnection = () => {
-    if (location.hostname === "localhost")
-    return io(url);
-else 
-    return io("/", {path: "/api/socket.io"})
-}
+  const userId = useSelector((state) => state.user?._id);
+
+  if (!userId) {
+    console.warn("⚠️ Socket not connected: userId missing");
+    return null;
+  }
+
+  return io(url, {
+    withCredentials: true,
+    auth: {
+      userId, // 🔑 THIS FIXES YOUR ONLINE ISSUE
+    },
+  });
+};
 
 export default createSocketConnection;
