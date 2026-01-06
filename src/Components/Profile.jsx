96e8258f5a3611416e0f25cb58ab96e26d4b3c62
Saravana <godsaran05@gmail.com>
2026-01-06 14:34:59 +0530

    completed cache

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,15 +1,33 @@
-
-import React from 'react'
-import EditProfile from './EditProfile'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from "react";
+import EditProfile from "./EditProfile";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { url } from "../utils/constants";
+import { addUser } from "../utils/userSlice";
 
 const Profile = () => {
-  const user = useSelector(store => store.user);
-  return (
-    user && <div>
-      <EditProfile user={user}/>
-    </div>
-  )
-}
+  const dispatch = useDispatch();
+  const user = useSelector((store) => store.user);
 
-export default Profile
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const res = await axios.get(`${url}/profile/view`, {
+          withCredentials: true,
+        });
+
+        dispatch(addUser(res.data)); // 🔑 update redux from Redis/DB
+      } catch (err) {
+        console.error("Failed to load profile", err.message);
+      }
+    };
+
+    fetchProfile();
+  }, [dispatch]);
+
+  if (!user) return null;
+
+  return <EditProfile user={user} />;
+};
+
+export default Profile;
